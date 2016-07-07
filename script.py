9c524d852d2c68e6c5fe6dc9e11261035b5cd468
suhas2go <suhas2go@gmail.com>
2016-07-07 22:19:46 +0530

    fixed the page load

--- script.py
+++ script.py
@@ -1,67 +1,84 @@
 import time
-import sys
-import getpass
+# import getpass
 from selenium import webdriver
 import bs4
 from tkinter import *
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
 
-f=open(r"C:\Users\Suhas\Documents\hashpw.txt")
-pw=f.read()
+f = open(r"C:\Users\Suhas\Documents\hashpw.txt")
+pw = f.read()
 
-flist=['Aditya Kanthale','Ashish Singh','Sambhav Kothari','Mohit Bagri'] #add your friends names here
-chrome_path=r"C:\Users\Suhas\Downloads\chromedriver.exe"
-ppath=r"C:\Users\Suhas\Downloads\phantomjs-2.1.1-windows\bin\phantomjs.exe"
+flist = ['Aditya Kanthale', 'Ashish Singh','Sambhav Kothari','Mohit Bagri']  # add your friends names here
+chrome_path = r"C:\Users\Suhas\Downloads\chromedriver.exe"
+ppath = r"C:\Users\Suhas\Downloads\phantomjs-2.1.1-windows\bin\phantomjs.exe"
 
-def leftclick(event,obj):
-		driver = webdriver.Chrome(chrome_path)
-		driver.maximize_window()
-		driver.get('http://www.facebook.com/login.php')
-		username=driver.find_element_by_id('email')
-		username.send_keys('suhas2go') #replace with your username
-		passw=driver.find_element_by_id('pass')
-		passw.send_keys(pw)
-		passw.submit()
-		chtbx=driver.find_element_by_class_name('_58al')
-		chtbx.send_keys(obj)
-		chtbx.submit()
 
-root=Tk()
+def leftclick(event, obj):
+	chrome_options = webdriver.ChromeOptions()
+	prefs = {"profile.default_content_setting_values.notifications" : 2}
+	chrome_options.add_experimental_option("prefs",prefs)
+	driver = webdriver.Chrome(executable_path=chrome_path, chrome_options=chrome_options)
+	driver.maximize_window()
+	driver.get('http://www.facebook.com/login.php')
+	username = driver.find_element_by_id('email')
+	username.send_keys('suhas2go')  # replace with your username
+	passw = driver.find_element_by_id('pass')
+	passw.send_keys(pw)
+	passw.submit()
+	chtbx = driver.find_element_by_class_name('_58al')
+	chtbx.send_keys(obj)
+	time.sleep(10)
+	chtbx.send_Keys(Keys.RETURN);
+
+
+root = Tk()
+root.title("Frilert")
 
 driver = webdriver.PhantomJS(ppath)
 driver.maximize_window()
 driver.get('http://www.facebook.com/login.php')
-username=driver.find_element_by_id('email')
-username.send_keys('suhas2go') #replace with your username
-passw=driver.find_element_by_id('pass')
+username = driver.find_element_by_id('email')
+username.send_keys('suhas2go')  # replace with your username
+passw = driver.find_element_by_id('pass')
 passw.send_keys(pw)
 passw.submit()
 
-time.sleep(15)
+delay=20
+#time.sleep(delay)
+try:
+    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'js_1g')))
+    print ("Page is ready!")
+except TimeoutException:
+    print ("Loading took too much time!")
 
-driver.save_screenshot('screen.png') 
-onl=[]
-html=driver.page_source
-soup=bs4.BeautifulSoup(html,'html.parser')
-frds=soup.findAll("li",attrs={'class':'_42fz'})
+
+# driver.save_screenshot('screen.png')
+onl = []
+html = driver.page_source
+soup = bs4.BeautifulSoup(html, 'html.parser')
+frds = soup.findAll("li", attrs={'class': '_42fz'})
 for frd in frds:
 	if frd.findAll('i'):
-		#all who are online 
-		for f in frd.findAll('div',attrs={'class':'_55lr'}):
-			if f.string in flist:
-				print (f.string)
-				onl.append(f.string)
+		# all who are online
+		for f in frd.findAll('div', attrs={'class': '_55lr'}):
+			print(f.string)
+			onl.append(f.string)
 
 if not onl:
-	l=Label(root,text='none is online on fb')
+	l = Label(root, text='none is online on fb')
 	l.pack()
 else:
-	ctr=0
-	butts=[]
+	ctr = 0
+	butts = []
 	for onlfrd in onl:
-		butts.append(Button(root,text=onlfrd))
+		butts.append(Button(root, text=onlfrd))
 	for b in butts:
 		b.pack()
-		b.bind('<Button-1>',lambda event, obj=b.config('text')[-1]: leftclick(event, obj))
-	
+		b.bind('<Button-1>', lambda event, obj=b.config('text')[-1]: leftclick(event, obj))
 
 root.mainloop()
